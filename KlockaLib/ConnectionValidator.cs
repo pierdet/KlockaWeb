2b12e9c76e5314172a93815bc41c91dee8f1c98e
Pierdet <antemxz@hotmail.com>
2020-01-17 21:39:26 +0100

    Implemented ping site

---
+++ KlockaLib/ConnectionValidator.cs
@@ -0,0 +1,55 @@
+﻿using KlockaLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace KlockaLib
+{
+    public class ConnectionValidator : IDisposable
+    {
+        private readonly Ping _pinger;
+        public ConnectionValidator()
+        {
+            _pinger = new Ping();
+        }
+
+        public ConnectionValidatorResponse Validate(string connection)
+        {
+            try
+            {
+                var reply = _pinger.Send(connection, 1000);
+
+                if (reply == null)
+                {
+                    return new ConnectionValidatorResponse
+                    {
+                        Connection = connection,
+                        Success = false,
+                        ErrorMessage = "could not receive a reply"
+                    };
+                }
+
+                return new ConnectionValidatorResponse
+                {
+                    Connection = connection,
+                    Success = reply.Status == IPStatus.Success
+                };
+            }
+            catch (Exception e)
+            {
+                return new ConnectionValidatorResponse
+                {
+                    Connection = connection,
+                    Success = false,
+                    ErrorMessage = $"unexpected error occurred: {e.Message}"
+                };
+            }
+        }
+
+        public void Dispose()
+        {
+            _pinger.Dispose();
+        }
+    }
+}
---
+++ KlockaLib/Models/ConnectionValidatorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KlockaLib.Models
+{
+    public class ConnectionValidatorResponse
+    {
+        public string Connection { get; set; }
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
