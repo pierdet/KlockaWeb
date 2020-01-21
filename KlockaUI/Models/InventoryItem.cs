fa206986010c1bee937ed337ce8dd2f62b33d2c4
Pierdet <antemxz@hotmail.com>
2020-01-21 19:45:42 +0100

    Added styling and navigation to inventories and inventorydetails. Added possibility to create inventories

---
+++ KlockaUI/Models/InventoryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KlockaUI.Models
+{
+    public class InventoryItem
+    {
+        [Required]
+        [StringLength(100,ErrorMessage = "Name is to long")]
+        [MinLength(1, ErrorMessage = "Name is to short")]
+        public string Name { get; set; }
+    }
+}
